1bdc5ab46e513ee1575dc0103d8c8061910ad134
he1kor <helkor.rightway@gmail.com>
2025-10-21 23:47:01 +0300

    feat: coutCPUState

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,53 @@
 #include "cpu.h"
 #include <format>
 
+template<uint16_t IMEM_SIZE, uint16_t DMEM_SIZE>
+void coutCPUState(const CPU<IMEM_SIZE, DMEM_SIZE>& cpu){
+    const int col1 = 20;
+    const int col2 = 15;
+    
+    std::cout << "==========================================" << std::endl;
+    std::cout << "|                CPU STATE               |" << std::endl;
+    std::cout << "==========================================" << std::endl;
+    
+    std::cout << "| " << std::left << std::setw(col1) << "ACC" 
+              << " | " << std::right << std::setw(col2) << cpu.getACC() << " |" << std::endl;
+    
+    std::cout << "| " << std::left << std::setw(col1) << "Program Counter" 
+              << " | " << std::right << std::setw(col2) << cpu.getPC() << " |" << std::endl;
+    
+    CPU<>::Instruction ir = cpu.getIR();
+    Assembly line(ir);
+    
+    std::cout << "|----------------------------------------|" << std::endl;
+    std::cout << "|               INSTRUCTION              |" << std::endl;
+    std::cout << "|----------------------------------------|" << std::endl;
+    
+    std::cout << "| " << std::left << std::setw(col1) << "Assembly" 
+              << " | " << std::right << std::setw(col2) << line.toString() << " |" << std::endl;
+    
+    std::stringstream hexStream;
+    hexStream << "0x" << std::hex << std::setw(4) << std::setfill('0') << ir.raw;
+    std::string hexStr = hexStream.str();
+    std::cout << "| " << std::left << std::setw(col1) << "Hex" 
+              << " | " << std::right << std::setw(col2) << hexStr << " |" << std::endl;
+    
+    std::cout << "| " << std::left << std::setw(col1) << "Decimal" 
+              << " | " << std::right << std::setw(col2) << std::dec << ir.raw << " |" << std::endl;
+    
+    std::cout << "|----------------------------------------|" << std::endl;
+    
+    std::string zeroFlagStr = cpu.getZ() ? "SET" : "CLEAR";
+    std::cout << "| " << std::left << std::setw(col1) << "Zero Flag" 
+              << " | " << std::right << std::setw(col2) << zeroFlagStr << " |" << std::endl;
+    
+    std::string carryFlagStr = cpu.getC() ? "SET" : "CLEAR";
+    std::cout << "| " << std::left << std::setw(col1) << "Carry Flag" 
+              << " | " << std::right << std::setw(col2) << carryFlagStr << " |" << std::endl;
+    
+    std::cout << "==========================================" << std::endl;
+}
+
 int main(){
 
     constexpr uint32_t IMEM_SIZE = 1024;
@@ -45,8 +92,7 @@ int main(){
 
     cpu.start();
     do {
-        std::cout << "ACC " << cpu.getACC() << "\tPC ";
-        std::cout << cpu.getPC() << "\n";
+        coutCPUState(cpu);
     } while ((cpu.step()));
 
     std::cout << "\n---FINSH---\n";
